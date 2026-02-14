3ef1721b830d086b97014612e0db29f3b1df2326
Sparsh-tyagi01 <sparshtyagi630@gmail.com>
2026-02-14 22:01:39 +0530

    add prisma models and connect auth with db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Home() {
   return (
